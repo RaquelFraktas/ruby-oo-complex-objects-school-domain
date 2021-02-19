fb0422ced008f14bc6d642c51bf3096329a5d69f
github username <github email address>
2021-02-19 12:34:04 -0500

    commit tree d4380c18ef97800372789f6b1572823acd1a5cf3

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+require 'pry'
+
+class School
+    attr_accessor :roster
+    
+
+    def initialize (roster)
+        @roster = Hash.new
+    end
+
+    def add_student(name,grade)
+        # @name = name ---it's bad because we're modifying the values. dont do it.
+        # @age= age -----same here.
+        
+        if @roster[grade]
+         @roster[grade] << name
+        
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(age)
+        #binding.pry
+        @roster[age]
+    end
+
+    def sort
+        @roster.each do |grade, name|
+            name.sort!
+        end
+    end
+
+end
+
+#hash["new_key"] = []
